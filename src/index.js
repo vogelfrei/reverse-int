4a24d53cf4eed00adbbd95bcbef07a4d8cc3156f
vogelfrei <mr.boot.sasha@gmail.com>
2021-02-24 22:39:39 +0300

    task solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n *= -1;
+    }
+    return Number(String(n).split("").reverse().join(""));
 }
